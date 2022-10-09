183ebce4a32746cbbf485eb8e9acb161f16832c8
yamasaki junpei <opss0622@gmail.com>
2022-10-09 13:14:04 +0900

    ソート機能実装中

--- src/js/userTable.js
+++ src/js/userTable.js
@@ -2,26 +2,38 @@
 
 window.addEventListener("DOMContentLoaded", () => {
   const url = "https://myjson.dit.upm.es/api/bins/4ga2";
-  const $userTableList = document.querySelector(".js-userTableList");
-
-  new CreateUserTable($userTableList, url);
-
+  const $userTable = document.querySelector(".js-userTable");
+  const sortStatus = {
+    asceding: "ascending",
+    descending: "descending"
+  }
   const config = {
     childList: true,
   };
+  const monitorCreatedUserTable = (mutationsList) => {
+    let mutationCount = 0;
+    // theadタグ要素とtbodyタグ要素が追加されたことにより、監視が2回発生している。
+    // それにより、mutationsListの中に2つのRecordが追加されてる。
+    // 1回目の監視の際に追加されたRecordにはtheadタグ要素に関する情報が入っているため、こちらは削除する。
+    // 2回目の監視の際に追加されたRecordにはtbodyタグ要素に関する情報が入っているため、こちらのみを検証していく。
+    mutationsList.splice(1, 1);
 
-  const callback = (mutationsList) => {
     mutationsList.forEach((mutation) => {
-      const childrNodes = mutation.addedNodes;
-      const $headingID = childrNodes[0];
-      const $sortBtn = $headingID.children[1];
+      const $tableHeadBlock = mutation.addedNodes[0];
+      const $tableBody = $tableHeadBlock.nextElementSibling;
+      const tableRow = $tableHeadBlock.childNodes[0];
+      const tableHead = tableRow.childNodes[0];
+      const $sortBtn = tableHead.childNodes[1];
       
       $sortBtn.addEventListener("click", (e) => {
-        new HandleSortUserTable(e);
+        new HandleSortUserTable(e, $tableBody, sortStatus, mutationCount);
+        mutationCount++;
       });
     });
   };
+  const observer = new MutationObserver(monitorCreatedUserTable);
+  
+  new CreateUserTable($userTable, url);
 
-  const observer = new MutationObserver(callback);
-  observer.observe($userTableList, config);
+  observer.observe($userTable, config);
 });
--- src/js/userTableClass.js
+++ src/js/userTableClass.js
@@ -1,8 +1,8 @@
 "use strict";
 
 class CreateUserTable {
-  constructor($userTableList, url) {
-    this.userTableList = $userTableList;
+  constructor($user_table, url) {
+    this.user_table = $user_table;
     this.url = url;
     this._fetchData();
   }
@@ -13,9 +13,9 @@ class CreateUserTable {
       this.request.open("GET", this.url, true);
       this.request.onload = () => {
         if (200 <= this.request.status && this.request.status < 300) {
-          setTimeout(() => {
-            resolve(this.request);
-          }, 3000);
+          resolve(this.request);
+          // setTimeout(() => {
+          // }, 3000);
         } else {
           reject(new Error(this.request.statusText));
         }
@@ -57,72 +57,151 @@ class CreateUserTable {
 
   _createTableList(userData) {
     this.userData = userData;
-    this.frag = document.createDocumentFragment();
-    this.ID = document.createElement("dt");
-    this.IDText = document.createElement("span");
-    this.IDSortBtn = document.createElement("button");
-    this.name = document.createElement("dd");
-    this.sex = document.createElement("dd");
-    this.age = document.createElement("dd");
-    this.ID.className = "userTable_term heading";
-    this.IDText.className = "userTable_term_text";
-    this.IDSortBtn.className = "userTable_term_sortBtn";
-    this.name.className = "userTable_description heading";
-    this.sex.className = "userTable_description heading";
-    this.age.className = "userTable_description heading";
-    this.IDText.textContent = "ID";
-    this.name.textContent = "名前";
-    this.sex.textContent = "性別";
-    this.age.textContent = "年齢";
-    this.ID.appendChild(this.IDText);
-    this.ID.appendChild(this.IDSortBtn);
-    this.frag.appendChild(this.ID);
-    this.frag.appendChild(this.name);
-    this.frag.appendChild(this.sex);
-    this.frag.appendChild(this.age);
+    this.thead = document.createElement("thead");
+    this.thSortBtn = document.createElement("button");
+    this.tbody = document.createElement("tbody");
+    this.tr = document.createElement("tr");
+    this.trFrag = document.createDocumentFragment();
+    this.tdFrag = document.createDocumentFragment();
+    this.thead.className = "user_table_headBlock";
+    this.tr.className = "user_table_row";
+    this.thSortBtn.className = "user_table_head_sortBtn";
+    this.tbody.className = "user_table_body";
+
+    for (let i = 0; i < this.userData.length - 1; i++) {
+      const th = document.createElement("th");
+      const thText = document.createElement("span");
+      th.className = "user_table_head";
+      thText.className = "user_table_head_text";
+
+      if (i === 0) {
+        thText.textContent = "ID";
+      } else if (i === 1) {
+        thText.textContent = "名前";
+      } else if (i === 2) {
+        thText.textContent = "性別";
+      } else if (i === 3) {
+        thText.textContent = "年齢";
+      }
+
+      if (i === 0) {
+        th.appendChild(thText);
+        th.appendChild(this.thSortBtn);
+      } else {
+        th.appendChild(thText);
+      }
+
+      this.trFrag.appendChild(th);
+    }
+    this.tr.appendChild(this.trFrag);
+    this.thead.appendChild(this.tr);
+    this.user_table.appendChild(this.thead);
 
     for (let i = 0; i < this.userData.length; i++) {
-      const id = document.createElement("dt");
-      const name = document.createElement("dd");
-      const sex = document.createElement("dd");
-      const age = document.createElement("dd");
-      
-      id.className = "userTable_term";
-      name.className = "userTable_description";
-      sex.className = "userTable_description";
-      age.className = "userTable_description";
-      id.textContent =  userData[i].id;
+      const tr = document.createElement("tr");
+      const id = document.createElement("td");
+      const name = document.createElement("td");
+      const sex = document.createElement("td");
+      const age = document.createElement("td");
+
+      tr.className = "user_table_row";
+      id.className = "user_table_description";
+      name.className = "user_table_description";
+      sex.className = "user_table_description";
+      age.className = "user_table_description";
+      id.setAttribute("data-id", userData[i].id);
+      age.setAttribute("data-age", userData[i].age);
+      id.textContent = userData[i].id;
       name.textContent = userData[i].name;
       sex.textContent = userData[i].sex;
       age.textContent = userData[i].age;
-      this.frag.appendChild(id);
-      this.frag.appendChild(name);
-      this.frag.appendChild(sex);
-      this.frag.appendChild(age);
-    }
+      tr.appendChild(id);
+      tr.appendChild(name);
+      tr.appendChild(sex);
+      tr.appendChild(age);
 
-    this.userTableList.appendChild(this.frag);
+      this.tdFrag.appendChild(tr);
+    }
+    this.tbody.appendChild(this.tdFrag);
+    this.user_table.appendChild(this.tbody);
   }
 }
 
 class HandleSortUserTable {
-  constructor(e) {
+  constructor(e, tableBody, sortStatus, mutationCount) {
     this.event = e;
+    this.tableBody = tableBody;
+    this.sortStatus = sortStatus;
+    this.mutationCount = mutationCount;
+    this.ascendingClassName = this.sortStatus.ascending;
+    this.descendingClassName = this.sortStatus.descending;
     this.CurrentTarget = this.event.currentTarget;
     this._switchAppearanceSortBtn();
   }
-  
+
   _switchAppearanceSortBtn() {
-    this.ascending = this.CurrentTarget.classList.contains('ascending');
-    this.descending = this.CurrentTarget.classList.contains('descending');
-    
+    this.flag = 0;
+    this.ascending = this.CurrentTarget.classList.contains(
+      this.ascendingClassName
+    );
+    this.descending = this.CurrentTarget.classList.contains(
+      this.descendingClassName
+    );
+
     if (!this.descending && !this.ascending) {
-      this.CurrentTarget.classList.add("ascending");
+      this.CurrentTarget.classList.add(this.ascendingClassName);
+      this.flag = 1;
+      this._sort(this.flag);
     } else if (this.ascending) {
-      this.CurrentTarget.classList.remove("ascending");
-      this.CurrentTarget.classList.add("descending");
+      this.CurrentTarget.classList.remove(this.ascendingClassName);
+      this.CurrentTarget.classList.add(this.descendingClassName);
+      this.flag = 2;
+      this._sort(this.flag);
     } else if (this.descending) {
-      this.CurrentTarget.classList.remove("descending");
+      this.CurrentTarget.classList.remove(this.descendingClassName);
+      this.flag = 0;
+      this._sort(this.flag);
     }
   }
+
+  _sort(flag) {
+    this.children = this.tableBody.children;
+    this.frag = document.createDocumentFragment();
+    let initialChildren;
+    
+    if (this.mutationCount === 0) {
+      initialChildren = this.children;
+    }
+
+    console.log(initialChildren);
+
+    if (flag === 0) {
+      [...initialChildren].forEach((child) => {
+        this.frag.appendChild(child);
+      });
+    } else if (flag === 1) {
+      this.sortedChildren = [...this.children].sort((prev, next) => {
+        this.prevId = prev.childNodes[0].dataset.id;
+        this.nextId = next.childNodes[0].dataset.id;
+        return this.prevId - this.nextId;
+      });
+    } else if (flag === 2) {
+      this.sortedChildren = [...this.children].sort((prev, next) => {
+        this.prevId = prev.childNodes[0].dataset.id;
+        this.nextId = next.childNodes[0].dataset.id;
+        return this.nextId - this.prevId;
+      });
+    }
+
+    if (flag != 0) {
+      this.sortedChildren.forEach((element) => {
+        this.frag.appendChild(element);
+      });
+    }
+    
+    this.tableBody.appendChild(this.frag);
+  }
 }
+
+// クリックしたらユーザーテーブルの要素を取得する
+// 取得した要素を配列に格納する
