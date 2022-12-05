f331fee5832d7f41d3dd6d970b2f177a3eeca091
yamasaki junpei <opss0622@gmail.com>
2022-12-05 20:26:00 +0900

    バリデーションに関するJSを修正、レイアウト調整

--- src/js/register.js
+++ src/js/register.js
@@ -1,6 +1,4 @@
-// 利用規約を読まずにはチェックボックスをチェックしてはいけない
-// デフォルトでpointer-event: noneにしておく必要がある
-// 利用規約を完全に読んだらpointer-event: visibleにしないといけない
+"use strict";
 
 // 利用規約の表示制御
 window.addEventListener("DOMContentLoaded", () => {
@@ -9,82 +7,84 @@ window.addEventListener("DOMContentLoaded", () => {
   const modalOverlay = document.querySelector(".js-modalOverlay");
   let ariaHiddenFlag = true;
 
-  function showModal(element) {
-    element.setAttribute("aria-hidden", "false");
-    element.classList.add("is-show");
-  }
-
-  function hideModal(element) {
-    element.setAttribute("aria-hidden", "true");
-    element.classList.remove("is-show");
-  }
-
-  // 利用規約のスクロール量検知
-  const observeScroll = (element) => {
-    const modalInner = element.children[0];
-    const modalBody = modalInner.children[0];
-    const modalBodyClientHeight = modalBody.clientHeight;
-    const modalBodyScrollHeight = modalBody.scrollHeight; // 画面に表示されていないコンテンツの高さも含む
-    
-    // スクロールが一番下に行ったらチェックボックスはcheckedになる
-    modalBody.onscroll = function() {
-      const scrollTop = this.scrollTop;
-      const diff = modalBodyScrollHeight - (modalBodyClientHeight + scrollTop);
-      
-      if (diff === 0) {
-        setTimeout(() => {
-          hideModal(element);
-          hideModal(modalOverlay);
-          ariaHiddenFlag = true;
-  
-          toEnableInputChecked();
-          toEnableSubmit();
-        }, 500);
-      }
-    };
-
-    const toEnableInputChecked = () => {
-      const checkbox = document.querySelector('input[aria-label="agree"]');
-
-      checkbox.setAttribute("checked", "true");
-      checkbox.classList.add('is-active');
-    };
-
-    const toEnableSubmit = () => {
-      const submit = document.querySelector('input[aria-label="submit"]');
-      
-      submit.removeAttribute("disabled");
-    };
-  };
-  observeScroll(modal);
-  
   triger.addEventListener("click", () => {
-    if (ariaHiddenFlag) {
-      showModal(modal);
-      showModal(modalOverlay);
-      ariaHiddenFlag = false;
-    }
+    const props = {
+      modal: modal,
+      modalOverlay: modalOverlay,
+      ariaHiddenFlag: ariaHiddenFlag,
+    };
+
+    const modalAnimation = new ModalAnimation(props);
+
+    modalAnimation.observeScroll();
+    ariaHiddenFlag = modalAnimation.toggleModal();
   });
-  
+
   modalOverlay.addEventListener("click", (e) => {
-    if (!ariaHiddenFlag) {
-      hideModal(modal);
-      hideModal(e.currentTarget);
-      ariaHiddenFlag = true;
-    }
+    const props = {
+      modal: modal,
+      modalOverlay: e.currentTarget,
+      ariaHiddenFlag: ariaHiddenFlag,
+    };
+
+    const modalAnimation = new ModalAnimation(props);
+
+    modalAnimation.observeScroll();
+    ariaHiddenFlag = modalAnimation.toggleModal();
   });
 });
 
-// 利用規約に同意したら送信できる
-window.addEventListener('DOMContentLoaded', () => {
+// inputエリアのエラーメッセージの表示制御
+window.addEventListener("DOMContentLoaded", () => {
+  const submit = document.querySelector('input[aria-label="submit"]');
+  let flag = false;
+  
+  const checkInputValue = (e, flag) => {
+    const formInputs = document.querySelectorAll(".js-formInput");
+    const usernamePattern = /^.{0,15}$/;
+    const emailPattern = /.{1,}@.*/;
+    const passwordPattern = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/;
+    const patterns = [usernamePattern, emailPattern, passwordPattern];
+
+    formInputs.forEach((formInput, index) => {
+      const value = formInput.value;
+      const errorMessage = formInput.nextElementSibling;
+
+      if (value === "" || value.match(patterns[index]) === null) {
+        e.preventDefault();
+        errorMessage.classList.add("is-show");
+        flag = true;
+      }
+
+      if (flag) {
+        // ユーザー名を監視
+        formInput.addEventListener("input", (e) => {
+          const matched = e.target.value.match(patterns[index]);
+          if(matched != null) {
+            errorMessage.classList.remove('is-show');
+          } else {
+            errorMessage.classList.add('is-show');
+          }
+        });
+      }
+    });
+  };
+
+  submit.addEventListener("click", (e) => {
+    checkInputValue(e, flag);
+  });
+});
+
+// 利用規約に同意したら送信できるようにする処理
+window.addEventListener("DOMContentLoaded", () => {
   const checkbox = document.querySelector('input[aria-label="agree"]');
   const submit = document.querySelector('input[aria-label="submit"]');
 
-  checkbox.addEventListener('change', () => {
-    if(checkbox.checked) {
+  checkbox.addEventListener("change", () => {
+    if (checkbox.checked) {
       submit.removeAttribute("disabled");
     } else {
       submit.setAttribute("disabled", "true");
     }
-  })
+  });
 });
---
+++ src/js/registerClass.js
@@ -0,0 +1,70 @@
+class ModalAnimation {
+  constructor(props) {
+    this.modal = props.modal;
+    this.modalOverlay = props.modalOverlay;
+    this.ariaHiddenFlag = props.ariaHiddenFlag;
+    
+  }
+  _showModal(element) {
+    element.setAttribute("aria-hidden", "false");
+    element.classList.add("is-show");
+  }
+
+  _hideModal(element) {
+    element.setAttribute("aria-hidden", "true");
+    element.classList.remove("is-show");
+  }
+
+  toggleModal() {
+    if(this.ariaHiddenFlag) {
+      this._showModal(this.modal);
+      this._showModal(this.modalOverlay);
+      this.ariaHiddenFlag = false;
+    } else {
+      this._hideModal(this.modal);
+      this._hideModal(this.modalOverlay);
+      this.ariaHiddenFlag = true;
+    }
+    
+    return this.ariaHiddenFlag;
+  }
+  
+  // 利用規約のスクロール量検知
+  observeScroll() {
+    this.modalInner = this.modal.children[0];
+    this.modalBody = this.modalInner.children[0];
+    const that = this;
+    
+    // スクロールが一番下に行ったらチェックボックスはcheckedになる
+    this.modalBody.onscroll = function() {
+      this.modalBodyClientHeight = this.clientHeight;
+      this.modalBodyScrollHeight = this.scrollHeight; // 画面に表示されていないコンテンツの高さも含む
+      this.scrollTop = this.scrollTop;
+      this.diff = this.modalBodyScrollHeight - (this.modalBodyClientHeight + this.scrollTop);
+      
+      if (this.diff === 0) {
+        setTimeout(() => {
+          that._hideModal(that.modal);
+          that._hideModal(that.modalOverlay);
+          that.ariaHiddenFlag = true;
+
+          that._toEnableInputChecked();
+          that._toEnableSubmit();
+        }, 500);
+      }
+    };
+  }
+
+  _toEnableInputChecked() {
+    this.checkbox = document.querySelector('input[aria-label="agree"]');
+
+    this.checkbox.setAttribute("checked", "true");
+    this.checkbox.removeAttribute("disabled");
+  };
+
+  _toEnableSubmit() {
+    this.submit = document.querySelector('input[aria-label="submit"]');
+
+    this.submit.removeAttribute("disabled");
+  };
+}
